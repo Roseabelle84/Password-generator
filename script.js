5fe7ba703a10e76269e88f60a50f76ca78f2e551
Belle <roseabelle84@gmail.com>
2022-05-29 13:51:20 -0500

    revamp

--- script.js
+++ script.js
@@ -8,53 +8,54 @@ function generatePassword() {
   // var number = confirm ()
   // var special characters= confirm()
   //var lengthPrompt =parseInt(prompt("Choose between")
-  var el_down = document.getElementById("ros");
-
-  function gfg_Run() {
-    el_down.innerHTML =
-      Math.random().toString(36).slice(2) +
-      Math.random().toString(36)
-        .toUpperCase().slice(2);
-
-
-
-    /* Function to generate combination of password */
-    function generateP() {
-      var pass = '';
-
-      for (let i = 1; i <= 8; i++) {
-        var char = Math.floor(Math.random()
-          * str.length + 1);
-
-        pass += str.charAt(char)
-      }
-
-      return pass;
-    }
-
-    function gfg_Run() {
-      el_down.innerHTML = generateP();
-    }
-
-
-
-
-
+  var passwordLength = Number.parseInt(prompt("Please, Enter password length?"))
+  if (Number.isNaN(passwordLength)) {
+    alert("Please, enter a number.")
+    return ""
   }
-  var password = "password is anything from letters to numbers"
-
-  /* code Ends here */
-
-  // Get references to the #generate element
-  var generateBtn = document.querySelector("#generate");
-
-  // Write password to the #password input
-  function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-    passwordText.value = password;
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Please, pick a number between 8 and 128")
+    return ""
   }
+  var validCharacters = ""
+  var lowerCase = confirm("Would you like a lowercase letter in your password")
+  if (lowerCase) {
+    validCharacters += "abcdefghijklmnopqrstuvwxyz"
+  }
+  var upperCase = confirm("Would you like a uppercase letter in your password")
+  if (upperCase) {
+    validCharacters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  }
+  var symbols = confirm("Would you like a symbol in your password?")
+  if (symbols) {
+    validCharacters += "!@#$%^&*"
+  }
+  if (validCharacters === "") {
+    alert("Please, select at least one type of character")
+    return ""
+  }
+  var password = ""
+
+  for (let i = 1; i <= passwordLength; i++) {
+    var index = Math.floor(Math.random() * validCharacters.length);
+
+    password += validCharacters.charAt(index)
+  }
+
+
+  return password
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
 
 //<button onclick = "gfg_Run()"> 
 
-
+generateBtn.addEventListener("click", writePassword)
